88b25f675f55482d8a077657315c1feac2b2cc8f
ldelrio1 <lucasdr.lgi@gmail.com>
2016-04-11 22:13:59 -0300

    Problema de Build Solucionado

--- libreria/src/main/java/ar/edu/untref/aydoo/Periodico.java
+++ libreria/src/main/java/ar/edu/untref/aydoo/Periodico.java
@@ -3,7 +3,7 @@ package ar.edu.untref.aydoo;
 public class Periodico extends Compra{
 	
 	public Periodico (String nombrePeriodico, String mes, double monto) {
-		super(nombrePeriodico, mes, monto);
+		super(nombrePeriodico, mes);
 	}
 
 }
--- libreria/src/main/java/ar/edu/untref/aydoo/Revista.java
+++ libreria/src/main/java/ar/edu/untref/aydoo/Revista.java
@@ -3,6 +3,6 @@ package ar.edu.untref.aydoo;
 public class Revista extends Compra {
 	
 	public Revista(String nombreRevista, String mes, double monto) {
-		super(nombreRevista, mes, monto);
+		super(nombreRevista, mes);
 	}
 }
