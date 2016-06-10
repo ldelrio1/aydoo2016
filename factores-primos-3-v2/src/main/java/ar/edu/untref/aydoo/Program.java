27184d0066d0880bd74aaf137527c2cce6e5ca45
ldelrio1 <lucasdr.lgi@gmail.com>
2016-06-10 20:54:20 -0300

    Agrega Program provisorio

---
+++ factores-primos-3-v2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -0,0 +1,16 @@
+package ar.edu.untref.aydoo;
+
+/**
+ * Created by lucas on 10/06/16.
+ */
+public class Program {
+
+    public static final void main(String args[]) throws ExcepcionIngresoInvalido {
+
+
+        CalculadorDeFactoresPrimos calculador = new CalculadorDeFactoresPrimos();
+        System.out.println(calculador.calcularFactoresPrimos(args[0]));
+
+    }
+
+}
