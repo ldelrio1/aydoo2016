035d12a8bfd8b08fcb82b338a153400db97a6bb5
ldelrio1 <lucasdr.lgi@gmail.com>
2016-06-14 00:57:12 -0300

    Completa Clase Program

--- factores-primos-3-v2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos-3-v2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -2,12 +2,17 @@ package ar.edu.untref.aydoo;
 
 import ar.edu.untref.aydoo.Excepciones.ExcepcionIngresoInvalido;
 
+import java.io.IOException;
+
 /**
  * Created by lucas on 10/06/16.
  */
 public class Program {
 
-    public static final void main(String args[]) throws ExcepcionIngresoInvalido {
+    /**
+     * Tratar de hacer Refactor
+     */
+    public static final void main(String args[]) throws ExcepcionIngresoInvalido, IOException {
 
         AnalizadorDeArgumentos analizador = new AnalizadorDeArgumentos();
 
@@ -15,8 +20,17 @@ public class Program {
 
         CalculadorDeFactoresPrimos calculador = new CalculadorDeFactoresPrimos();
         calculador.calcularFactoresPrimos(analizador.getNumero());
-        System.out.println(calculador.getFactoresPrimos(analizador.getOrden()));
 
+        FormatoDeSalida formatoDeSalida = new FormatoDeSalida();
+        String salida = formatoDeSalida.salidaConFormato(analizador.getNumero(),
+                calculador.getFactoresPrimos(analizador.getOrden()), analizador.getFormato());
+
+        if (analizador.getArchivo()== null){
+            System.out.println(salida);
+        }else {
+            EscrituraEnArchivo escribir = new EscrituraEnArchivo();
+            escribir.escribirEnArchivo(analizador.getArchivo(), salida);
+        }
     }
 
 }
