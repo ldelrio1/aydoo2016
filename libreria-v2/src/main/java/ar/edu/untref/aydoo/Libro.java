7a0c651efed6e2efb2c1b429b31bd1c6b7a0649e
ldelrio1 <lucasdr.lgi@gmail.com>
2016-06-23 16:02:45 -0300

    Agrega Producto Libro

---
+++ libreria-v2/src/main/java/ar/edu/untref/aydoo/Libro.java
@@ -0,0 +1,14 @@
+package ar.edu.untref.aydoo;
+
+/**
+ * Created by lucas on 23/06/16.
+ */
+public class Libro extends Producto{
+
+
+    public Libro(String nombre, double precio) {
+        super(nombre, precio);
+    }
+
+
+}
---
+++ libreria-v2/src/test/java/ar/edu/untref/aydoo/LibroTest.java
@@ -0,0 +1,27 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by lucas on 23/06/16.
+ */
+public class LibroTest {
+
+    @Test
+    public void creaLibroVerificaNombre(){
+
+        Libro elSeniorDeLosAnillos = new Libro("El Señor de los Anillos", 170);
+
+        Assert.assertEquals( "El Señor de los Anillos" , elSeniorDeLosAnillos.getNombre());
+    }
+
+    @Test
+    public void creaLibroVerificaPrecio(){
+
+        Libro elHobbit = new Libro("El Hobbit", 200);
+
+        Assert.assertEquals( 200, elHobbit.getPrecio(),0.01);
+    }
+
+}
